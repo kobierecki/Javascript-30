b8c566c46ad51b7747f50ccaa081f05ac940ecee
Oskar Kobierecki <oskar.kobierecki@o2.pl>
2018-01-18 20:30:13 +0100

    v3

--- DrumKit/script.js
+++ DrumKit/script.js
@@ -1,7 +1,7 @@
 
 window.addEventListener('keydown', function(e){
  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
- if(!audtio) return;
+ if(!audio) return;
  console.log(audio);
  audio.currentTime = 0;
  audio.play();
